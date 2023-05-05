f7f47f5f4ec9f5fd234494b5856f94b1fcc4513d
Chetana Mali <65480069+ChetanaMali@users.noreply.github.com>
2023-05-06 00:30:14 +0530

    Instantiate
    Try to instantiate rings on trigger

--- Assets/Scripts/AddRing.cs
+++ Assets/Scripts/AddRing.cs
@@ -4,6 +4,29 @@ using UnityEngine;
 
 public class AddRing : MonoBehaviour
 {
-    
-
+    [SerializeField] GameObject m_RingPrefab;
+    GameObject childobject;
+    [SerializeField] int m_NoOfRings = 0;
+    private void Start()
+    {
+        childobject = m_RingPrefab;
+        childobject.transform.SetParent(m_RingPrefab.transform);
+    }
+    void InstantiateRing()
+    {
+        
+        for(int i=0; i< m_NoOfRings; i++)
+        {
+            
+            childobject.transform.position = new Vector3(m_RingPrefab.transform.position.x, (m_RingPrefab.transform.position.y + (i - 0.3f)), m_RingPrefab.transform.position.z); 
+            Instantiate(childobject, childobject.transform.position, Quaternion.identity);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Ring")
+        {
+            InstantiateRing();
+        }
+    }
 }
